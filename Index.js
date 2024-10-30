508d3b87b381255981631fb8134785834da0dba1
ganeshjangam9666 <jangamganesh500@gmail.com>
2024-10-30 22:18:13 +0530

    Episode-01

---
+++ Index.js
@@ -0,0 +1,3 @@
+const heading=React.createElement("h1",{id: "heading1"},"This is h1 Tag from React!!!");
+const root=ReactDOM.createRoot(document.getElementById("container"));
+root.render(heading);
